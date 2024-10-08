7f26b38282a4c458f6de14d3bb5a4bc207e00f79
JeremyvanScharenburg <jeremy.vanscharenburg@student.hu.nl>
2024-10-08 11:07:28 +0200

    Update memory.php

--- memory.php
+++ memory.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Memory</title>
+    <title>Memry</title>
     <link rel="stylesheet" href="./styling/memory.css">
 </head>
 <body>
