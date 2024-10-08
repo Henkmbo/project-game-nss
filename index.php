ec0df120ee6ebac1564e3174cbcbb74f64399a2d
younes253 <younes.elmimouni@student.hu.nl>
2024-10-08 09:20:38 +0200

    beetje aan de homepage gewerkt

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
+<link rel="stylesheet" href="./styling/younes.css">
 </head>
 <body>
+ <section class="hero">
+    <h1> Welkom bij onze memory game</h1>
+    <p>test je back end kennis met onze memory</p>
+</section>
+
     <a href="./login.php">Login</a>
 </body>
 </html>
