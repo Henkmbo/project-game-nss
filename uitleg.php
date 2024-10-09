91c7be0b5ba99e92089b32c5bbd9bff162ddede8
JeremyvanScharenburg <jeremy.vanscharenburg@student.hu.nl>
2024-10-09 10:51:55 +0200

    .

--- uitleg.php
+++ uitleg.php
@@ -12,20 +12,20 @@
     </div>
     <section class="boxes">
     <div class="basis">
-        <h2><b>Memory! Het spel waar je geheugen getest wordt. Simpel maar effectief.</b></h2>
-        <h3>Durf jij je kennis te testen?</h3>
-        <h3> Aantal spelers: 2 of meer <br><br>
+        <h1><b>Memory! Het spel waar je geheugen getest wordt. Simpel maar effectief.</b></h1>
+        <h2>Durf jij je kennis te testen?
+        <h2> Aantal spelers: 2 of meer <br><br>
 
 <b>Doel van het spel:</b><br><br>
 Het doel van Memory is om zoveel mogelijk paren van dezelfde kaarten te vinden en op te slaan in je verzameling.
 <br><br>
 <b>Spelverloop:</b><br>
 Het spelbord verschijnt online met een aantal kaarten die met de achterkant naar boven liggen. Het aantal kaarten varieert per spel, afhankelijk van het niveau of de instellingen.
-<br><br></div>
+<br><br></h2></div>
 <div class="basis">
-De eerste speler klikt op twee kaarten om deze om te draaien en de afbeeldingen zichtbaar te maken:
+<h2>De eerste speler klikt op twee kaarten om deze om te draaien en de afbeeldingen zichtbaar te maken:
 Als de kaarten een paar vormen (dezelfde afbeelding of tekst), blijven de kaarten omgedraaid en worden aan de verzameling van de speler toegevoegd. De speler mag daarna opnieuw twee kaarten omdraaien.
-Als de kaarten niet overeenkomen, draaien ze automatisch terug naar hun oorspronkelijke positie (met de achterkant naar boven), en is de volgende speler aan de beurt.
+<br>Als de kaarten niet overeenkomen, draaien ze automatisch terug naar hun oorspronkelijke positie (met de achterkant naar boven), en is de volgende speler aan de beurt.
 Onthouden is de sleutel: Probeer te onthouden welke kaarten je hebt omgedraaid, zodat je bij je volgende beurt gemakkelijker een paar kunt vinden.
 <br>
 Het spel gaat door totdat alle paren zijn gevonden.
@@ -33,7 +33,7 @@ Het spel gaat door totdat alle paren zijn gevonden.
 <b>Einde van het spel:</b><br>
 Wanneer alle paren zijn gevonden, wordt het spel automatisch beëindigd en krijgt elke speler het aantal behaalde paren te zien. De speler met de meeste paren wint.
 <br>
-<h2><b>Veel plezier met het online Memory spel!</b></h2></h3>
+<h2><b>Veel plezier met het online Memory spel!</b></h2></h2>
 <button class="home-btn" onclick="window.location.href = 'index.php';">HOME</button>
     </div>
 </section>
