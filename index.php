d8de8395ef4cf4a40b7600d4194ab6727b26ce17
younes253 <younes.elmimouni@student.hu.nl>
2024-10-09 10:41:16 +0200

    nav bar gemaakt

--- index.php
+++ index.php
@@ -5,7 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
 <link rel="stylesheet" href="./styling/younes.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./uitleg.php">Instructies</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="memory.php">Memomry</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">log in</a>
+        </li>
+        <li class="nav-item">
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 <body>
 <section class="hero">
         <h1>Welkom bij onze Memory Game!</h1>
@@ -36,5 +60,7 @@
     </section>
     <a href="./login.php">Login</a>
     <a href="./uitleg.php">Uitleg</a>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
