f64718234f5c71e893c9ffa4d7f9846622f12ab7
younes253 <younes.elmimouni@student.hu.nl>
2024-10-09 09:05:59 +0200

    homepage afgemaakt

--- index.php
+++ index.php
@@ -7,14 +7,34 @@
 <link rel="stylesheet" href="./styling/younes.css">
 </head>
 <body>
- <section class="hero">
-    <h1> Welkom bij onze memory game</h1>
-    <p>test je back end kennis met onze memory</p>
-</section>
-<div class="flexcontainer">
-    
+<section class="hero">
+        <h1>Welkom bij onze Memory Game!</h1>
+        <p>Test je kennis met het klassieke memory spel.</p>
+    </section>
 
-</div>
+   
+    <section class="boxes">
+        <div class="box">
+        <img src="./afbeeldingen/memory.jpeg" >
+            <h2>Speel nu</h2>
+            <p>Test je kennis over Backend Development door een uitdagend Memory spel te spelen. 
+            Match de kaarten met termen zoals databases, API's, server-side programming, en meer.
+            Ontdek hoe goed je de kernconcepten begrijpt en verbeter je vaardigheden tijdens het spel</p>
+    
+            <a href="./memory.php" class="play-btn">Speel nu</a>
+        </div>
+
+    
+        <div class="box">
+        <img src="./afbeeldingen/instructies.png" class="image2" >
+            <h2>Instructies</h2>
+            <p>Het doel van dit Memory spel is om alle paren van backend-technologieën te matchen. 
+            Klik op een kaart om deze te onthullen en zoek naar de bijbehorende term. Wil je meer 
+            leren over deze technologieën? Klik hieronder voor een uitgebreide uitleg!</p>
+            <img href="">
+            <a href="./uitleg.php" class="learn-btn">Lees meer</a>
+        </div>
+    </section>
     <a href="./login.php">Login</a>
     <a href="./uitleg.php">Login</a>
 </body>
